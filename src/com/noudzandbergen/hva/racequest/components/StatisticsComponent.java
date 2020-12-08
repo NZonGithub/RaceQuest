a59ed7a0a4b67a329baa88e317eeee1c4c6aa4b0
DungeYT <ruben.van.vliet@hva.nl>
2020-12-08 15:05:22 +0100

    I made the text rendering for the Statistics

--- src/com/noudzandbergen/hva/racequest/components/StatisticsComponent.java
+++ src/com/noudzandbergen/hva/racequest/components/StatisticsComponent.java
@@ -1,10 +1,15 @@
 package com.noudzandbergen.hva.racequest.components;
 
 import com.noudzandbergen.hva.racequest.RaceQuest;
+import processing.core.PConstants;
 import processing.core.PGraphics;
 import processing.core.PVector;
 
 public class StatisticsComponent {
+	private int row1 = 100;
+	private int row2 = 200;
+	private int row3 = 300;
+	private int rowAnwsers = 400;
 
 	private final RaceQuest game;
 
@@ -17,7 +22,24 @@ public class StatisticsComponent {
 	}
 
 	public void draw(PGraphics g, PVector size, float time) {
-		g.fill(0xffffffff);
+
+		g.push();
+
+		// Fill background
+		g.fill(0xff000000);
 		g.rect(0, 0, size.x, size.y);
+
+		// Draw some random text
+		g.fill(0xffffffff);
+		g.textSize(30);
+		g.textAlign(PConstants.CORNER, PConstants.CENTER);
+		g.text("Current Place:", 0, row1);
+		g.text("Current Highscore:", 0, row2);
+		g.text("Laps:", 0, row3);
+		g.text("highscore.place", rowAnwsers, row1);
+		g.text("highscore", rowAnwsers, row2);
+		g.text("laps.remaing, /3", rowAnwsers, row3);
+		g.pop();
+
 	}
 }
