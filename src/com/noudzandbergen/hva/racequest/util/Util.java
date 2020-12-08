27085228b4b0a928bb52a8aad66949b549bebc0f
NZonGithub <NZGamesNL@gmail.com>
2020-12-08 12:05:00 +0100

    Add a utility class for random methods that can be used everywhere

---
+++ src/com/noudzandbergen/hva/racequest/util/Util.java
@@ -0,0 +1,20 @@
+package com.noudzandbergen.hva.racequest.util;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+public class Util {
+
+	public static <T> T getRandomItem(List<T> list) {
+		return list.get((int) (Math.random() * list.size()));
+	}
+
+	public static <T> T tryOr(Supplier<T> supplier, T alt) {
+		try {
+			return supplier.get();
+		} catch (Throwable err) {
+			return alt;
+		}
+	}
+
+}
